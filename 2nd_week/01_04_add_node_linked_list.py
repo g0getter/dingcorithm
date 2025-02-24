42fb3860dccaaa43400281c43b493954c5ea756e
나경 <nagyeong.yeo1023@gmail.com>
2025-02-24 15:20:20 +0900

    feat: LinkedList 구현(추가, 삭제 등)

--- 2nd_week/01_04_add_node_linked_list.py
+++ 2nd_week/01_04_add_node_linked_list.py
@@ -1,8 +1,10 @@
+from uuid import getnode
+
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-
 
 class LinkedList:
     def __init__(self, value):
@@ -16,6 +18,7 @@ class LinkedList:
 
     def print_all(self):
         cur = self.head
+        print("<all nodes>")
         while cur is not None:
             print(cur.data)
             cur = cur.next
@@ -24,11 +27,30 @@ class LinkedList:
         cur = self.head
         for _ in range(0, index):
             cur = cur.next
-        print("index", index, ":", cur.data)
+        # print("index", index, ":", cur.data)
         return cur
+
+    def add_node(self, index, value):
+        new_node = Node(value)
+
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            return
+
+        # index != 0
+        previous_node = self.get_node(index-1) # 삽입 직전 노드로 감
+
+        # 변경
+        new_node.next = previous_node.next
+        previous_node.next = new_node
+
+        return
 
 linked_list = LinkedList(5)
 linked_list.append(12)
-
+linked_list.append(17)
 linked_list.print_all()
-linked_list.get_node(1) # -> 5를 들고 있는 노드를 반환해야 합니다!
+
+linked_list.add_node(3, 7)
+linked_list.print_all()
---
+++ 2nd_week/01_05_delete_node_linked_list.py
@@ -0,0 +1,70 @@
+from uuid import getnode
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+
+    def append(self, value):
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(value)
+
+    def print_all(self):
+        cur = self.head
+        print("<all nodes>")
+        while cur is not None:
+            print(cur.data)
+            cur = cur.next
+
+    def get_node(self, index):
+        cur = self.head
+        for _ in range(0, index):
+            cur = cur.next
+        # print("index", index, ":", cur.data)
+        return cur
+
+    def add_node(self, index, value):
+        new_node = Node(value)
+
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            return
+
+        # index != 0
+        previous_node = self.get_node(index-1) # 삽입 직전 노드로 감
+
+        # 변경
+        new_node.next = previous_node.next
+        previous_node.next = new_node
+
+        return
+
+    def delete_node(self, index):
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        # index != 0
+        previous_node = self.get_node(index-1)
+        previous_node.next = previous_node.next.next
+
+        return
+
+linked_list = LinkedList(5)
+linked_list.append(12)
+linked_list.append(17)
+linked_list.print_all()
+
+linked_list.add_node(3, 7)
+linked_list.print_all()
+
+linked_list.delete_node(0)
+linked_list.print_all()
